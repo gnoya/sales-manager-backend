541d835023abce652b22af898de4cec3c9f6c778
Gabriel Noya <gabrielnoya95@gmail.com>
2023-01-21 22:45:45 -0400

    Login working and JWT check endpoint working

---
+++ services/auth/prisma/migrations/20230122024105_secret_to_string_text/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `JWT` MODIFY `secret` TEXT NOT NULL;
--- services/auth/src/routes/auth.routes.ts
+++ services/auth/src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import AuthController from '../controllers/auth.controller'
 const authController = new AuthController()
 const authRoutes = Router()
 
-authRoutes.post('/', authController.login)
+authRoutes.post('/login', authController.login)
 authRoutes.get('/token/:token', authController.checkToken)
 
 export default authRoutes
